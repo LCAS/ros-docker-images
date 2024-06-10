5c3dccbe89915539b203d2dc3cec5934678316bd
Marc Hanheide <marc@hanheide.net>
2024-06-10 11:00:33 +0100

    change to blackbox wm

--- start-turbovnc.sh
+++ start-turbovnc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export VGL_FPS=30
 export VGL_DISPLAY=egl
-/opt/TurboVNC/bin/vncserver -vgl -depth 24  -securitytypes TLSNone,X509None,None -wm xfce4-session > /tmp/vnc.log 2>&1
+/opt/TurboVNC/bin/vncserver -vgl -depth 24  -securitytypes TLSNone,X509None,None -wm blackbox > /tmp/vnc.log 2>&1
 screen -dmS vnc bash -c '/usr/local/novnc/noVNC-1.4.0/utils/novnc_proxy --vnc localhost:5901 --listen 5801 2>&1 | tee /tmp/novnc.log'
