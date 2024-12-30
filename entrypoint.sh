91feeb2dc6e50e66a32a2684d1b0cc6e2e56a47c
Marc Hanheide <marc@hanheide.net>
2024-12-30 21:47:16 +0000

    redirect stderr to log files in entrypoint and start-turbovnc scripts for better debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 set -xe
 # hand over to nvidia if present
 if [ -r /opt/nvidia/nvidia_entrypoint.sh ]; then
-    exec /opt/nvidia/nvidia_entrypoint.sh "$@"
+    exec /opt/nvidia/nvidia_entrypoint.sh "$@" 2>/tmp/entrypoint.log
 else
-    bash /opt/nvidia/entrypoint.d/90-turbovnc.sh
+    bash /opt/nvidia/entrypoint.d/90-turbovnc.sh 2>/tmp/entrypoint.log
     exec "$@"
 fi
--- start-turbovnc.sh
+++ start-turbovnc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "starting turbovnc"
+#echo "starting turbovnc" > /tmp/start.log
 sudo rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 screen -dmS turbovnc bash -c 'VGL_DISPLAY=egl VGL_FPS=30 /opt/TurboVNC/bin/vncserver :1 -depth 24 -noxstartup -securitytypes TLSNone,X509None,None 2>&1 | tee /tmp/vnc.log; read -p "Press any key to continue..."'
 # wait for VNC to be running
-echo "waiting for turbovnc to be up (display not available messages are expected during this time)"
-while ! xdpyinfo -display :1 > /dev/null; do
+#echo "waiting for turbovnc to be up (display not available messages are expected during this time)" 
+while ! xdpyinfo -display :1 > /dev/null 2>&1; do
     sleep 1
 done
 sleep 1
-echo "starting xfce4"
+#echo "starting xfce4"
 screen -dmS xfce4 bash -c 'DISPLAY=:1 /usr/bin/xfce4-session 2>&1 | tee /tmp/xfce4.log'
-echo "starting novnc ${NOVNC_VERSION}"
+#echo "starting novnc ${NOVNC_VERSION}"
 screen -dmS novnc bash -c '/usr/local/novnc/noVNC-${NOVNC_VERSION}/utils/novnc_proxy --vnc localhost:5901 --listen 5801 2>&1 | tee /tmp/novnc.log'
